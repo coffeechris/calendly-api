ecaa61d43632be64d9e8ffe26772817ea1b699b6
coffeechris <chris@chrisjordan.ca>
2018-05-01 21:06:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Release](https://jitpack.io/v/coffeechris/Repo.svg)]
+(https://jitpack.io/#coffeechris/Repo)
---
+++ src/main/java/com/github/coffeechris/calendlyapi/webhook/RequestBody.java
@@ -0,0 +1,8 @@
+package com.github.coffeechris.calendlyapi.webhook;
+
+import lombok.Data;
+
+@Data
+public class RequestBody {
+    private String event;
+}
