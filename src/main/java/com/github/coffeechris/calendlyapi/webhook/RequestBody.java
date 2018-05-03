33173a63691eb6151e570826c01bd65018401707
coffeechris <chris@chrisjordan.ca>
2018-05-02 20:33:41 -0700

    continued to extend the request body

--- src/main/java/com/github/coffeechris/calendlyapi/webhook/RequestBody.java
+++ src/main/java/com/github/coffeechris/calendlyapi/webhook/RequestBody.java
@@ -19,6 +19,9 @@ public class RequestBody {
         @JsonProperty("event_type")
         private EventType eventType;
         private Event event;
+        private Invitee invitee;
+        @JsonProperty("questions_and_answers")
+        private List<QuestionAnswer> questionsAndAnswers;
 
         @Data
         public static class EventType {
@@ -38,11 +41,37 @@ public class RequestBody {
 
         @Data
         public static class Event {
-            String uuid;
+            private String uuid;
             @JsonProperty("assigned_to")
-            List<String> assignedTo;
+            private List<String> assignedTo;
             @JsonProperty("extended_assigned_to")
-            List<ExtendedAssignedTo> extendedAssignedTo;
+            private List<ExtendedAssignedTo> extendedAssignedTo;
+            @JsonProperty("start_time")
+            private String startTime;
+            @JsonProperty("start_time_pretty")
+            private String startTimePretty;
+            @JsonProperty("invitee_start_time")
+            private String inviteeStartTime;
+            @JsonProperty("invitee_start_time_pretty")
+            private String inviteeStartTimePretty;
+            @JsonProperty("end_time")
+            private String endTime;
+            @JsonProperty("end_time_pretty")
+            private String endTimePretty;
+            @JsonProperty("invitee_end_time")
+            private String inviteeEndTime;
+            @JsonProperty("invitee_end_time_pretty")
+            private String inviteeEndTimePretty;
+            @JsonProperty("created_at")
+            private String createdAt;
+            private String location;
+            private boolean canceled;
+            @JsonProperty("canceler_name")
+            private String cancelerName;
+            @JsonProperty("cancel_reason")
+            private String cancelReason;
+            @JsonProperty("canceled_at")
+            private String canceledAt;
 
             @Data
             public static class ExtendedAssignedTo {
@@ -51,79 +80,49 @@ public class RequestBody {
                 private boolean primary;
             }
         }
+
+        @Data
+        public static class Invitee {
+            private String uuid;
+            @JsonProperty("first_name")
+            private String firstName;
+            @JsonProperty("last_name")
+            private String lastName;
+            private String name;
+            private String email;
+            private String timezone;
+            @JsonProperty("created_at")
+            private String createdAt;
+            @JsonProperty("is_reschedule")
+            private boolean isReschedule;
+            private List<Payment> payments;
+            private boolean canceled;
+            @JsonProperty("canceler_name")
+            private String cancelerName;
+            @JsonProperty("cancel_reason")
+            private String cancelReason;
+            @JsonProperty("canceled_at")
+            private String canceledAt;
+
+            @Data
+            public static class Payment {
+                private String id;
+                private String provider;
+                private float amount;
+                private String currency;
+                private String terms;
+                private boolean successful;
+            }
+        }
+
+        @Data
+        public static class QuestionAnswer {
+            private String question;
+            private String answer;
+        }
     }
 /*
     "payload":{
-
-      "event":{
-        "uuid":"BBBBBBBBBBBBBBBB",
-        "assigned_to":[
-          "Jane Sample Data"
-        ],
-        "extended_assigned_to":[
-          {
-            "name":"Jane Sample Data",
-            "email":"user@example.com",
-            "primary":false
-          }
-        ],
-        "start_time":"2018-03-14T12:00:00Z",
-        "start_time_pretty":"12:00pm - Wednesday, March 14, 2018",
-        "invitee_start_time":"2018-03-14T12:00:00Z",
-        "invitee_start_time_pretty":"12:00pm - Wednesday, March 14, 2018",
-        "end_time":"2018-03-14T12:15:00Z",
-        "end_time_pretty":"12:15pm - Wednesday, March 14, 2018",
-        "invitee_end_time":"2018-03-14T12:15:00Z",
-        "invitee_end_time_pretty":"12:15pm - Wednesday, March 14, 2018",
-        "created_at":"2018-03-14T00:00:00Z",
-        "location":"The Coffee Shop",
-        "canceled":false,
-        "canceler_name":null,
-        "cancel_reason":null,
-        "canceled_at":null
-      },
-      "invitee":{
-        "uuid":"AAAAAAAAAAAAAAAA",
-        "first_name":"Joe",
-        "last_name":"Sample Data",
-        "name":"Joe Sample Data",
-        "email":"not.a.real.email@example.com",
-        "timezone":"UTC",
-        "created_at":"2018-03-14T00:00:00Z",
-        "is_reschedule":false,
-        "payments":[
-          {
-            "id":"ch_AAAAAAAAAAAAAAAAAAAAAAAA",
-            "provider":"stripe",
-            "amount":1234.56,
-            "currency":"USD",
-            "terms":"sample terms of payment (up to 1,024 characters)",
-            "successful":true
-          }
-        ],
-        "canceled":false,
-        "canceler_name":null,
-        "cancel_reason":null,
-        "canceled_at":null
-      },
-      "questions_and_answers":[
-        {
-          "question":"Skype ID",
-          "answer":"fake_skype_id"
-        },
-        {
-          "question":"Facebook ID",
-          "answer":"fake_facebook_id"
-        },
-        {
-          "question":"Twitter ID",
-          "answer":"fake_twitter_id"
-        },
-        {
-          "question":"Google ID",
-          "answer":"fake_google_id"
-        }
-      ],
       "questions_and_responses":{
         "1_question":"Skype ID",
         "1_response":"fake_skype_id",
